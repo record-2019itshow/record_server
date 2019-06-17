e8482a4e4bc9d361bf0c1554126a2800e5875d24
Seungyeon Song <song99pro@gmail.com>
2019-06-17 13:48:58 +0900

    multer 추가(pull 받고 npm install 하기)

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 // var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var multer  = require('multer');
 
 app.use(session({
   secret: '@#@$MYSIGN#@$#$', // 우선 임의로 키 설정
@@ -28,11 +29,25 @@ mongoose.connect("mongodb://localhost/record", { useNewUrlParser: true });
 // Define Model
 import { Records, HashTags, Members } from './models/RecordSchema';
 
+// var Member = RecordModel.Member;
+// var HashTag = RecordModel.HashTag;
+// var Record = RecordModel.Record;
+
 var SchemaController = require('./models/SchemaController');
 app.use('/record', SchemaController);
 
-require('./routes/record')(app, Records, HashTags);
-require('./routes/users')(app, Members);
-
-
+require('./routes/record/dayRecord')(app ,Records);
+require('./routes/users/users')(app ,Members);
+//app.use('/getDayRecord', dayRecord);
+/*
+app.get('/getDayRecord/:id/:time', async(req, res) => {
+  console.log("connect");
+  var result = await records.findOne({ id: req.params.id, time: req.params.time });
+  //if(result) res.status(200).json(result);
+  //else res.status(400).json({message: 'No Record'});
+  //else res.send('No Record');
+  console.log(req.params.id);
+  res.send(result);
+ });
+*/
 module.exports = app;
