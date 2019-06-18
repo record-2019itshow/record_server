bf7d3e67e69ef0727cc64b7d133b4802d5b6dd78
Seungyeon Song <song99pro@gmail.com>
2019-06-18 19:17:26 +0900

    괄호 오휴

--- routes/addRecord.js
+++ routes/addRecord.js
@@ -23,14 +23,11 @@ module.exports = (app, Records, Members) => {
         reqReco.record_key = randomString(10);
         //임시로 랜덤 스트링 10개 생성
 
-
         //레코드 저장
-        reqReco.save(async(err)=>{
+        await reqReco.save(async(err)=>{
                 if(err) res.status(400).json({message: "error!"});
                 res.status(200).json(reqReco);
             });
-        });
-        
 
         //Members 에 해시태그 추가
         await Members.findOne({id : reqid}, function (err, rawContent){
@@ -41,7 +38,8 @@ module.exports = (app, Records, Members) => {
             });
         });
 
-    }
+    });
+}
 
     function randomString(len) {
         var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
