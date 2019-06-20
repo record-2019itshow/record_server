72e9356b231fe2c7943e6b48d41395035fed05f4
ChoiYura <s2017i19@e-mirim.hs.kr>
2019-06-20 18:24:06 +0900

    getSingleRecord 추가

--- routes/record.js
+++ routes/record.js
@@ -5,17 +5,18 @@ module.exports = (app, Records) => {
   if(result) res.status(200).json(result);
   else res.status(400).json('Not Found Record');
  })
- .get('/getHashRecord/:id/:record_key/:hashtags', async (req, res) => {
-  await Records.find({ id: req.params.id, record_key: req.params.record_key }, async (err, subRes)=> {
-   if(err) res.status(400).json({ error });
-   else if(!subRes) res.status(400).json('Not Found Record');
-   var result = await Records.findOne({ record_key: req.params.record_key, hashtags: req.params.hashtags });
-   if(result) res.status(200).json(result);
-   else res.status(400).json('Not Found Record');
-  })
+ .get('/getHashRecord/:id/:hashtags', async (req, res) => {
+  var result = await Records.find({ id: req.params.id, hashtags: '#'+req.params.hashtags });
+  if(result) res.status(200).json(result);
+  else res.status(400).json('Not Found Record');
  })
  .get('/getAllRecord/:id', async (req, res) => {
   var result = await Records.find({ id: req.params.id });
+  if(result) res.status(200).json(result);
+  else res.status(400).json('Not Found Record');
+ })
+ .get('/getSingleRecord/:id/:record_key', async (req, res) => {
+  var result = await Records.find({ id: req.params.id, record_key: req.params.record_key });
   if(result) res.status(200).json(result);
   else res.status(400).json('Not Found Record');
  });
